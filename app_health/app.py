459ff23e3244fb304afd69e80ea9d734440976f5
jurioste4 <jurioste4@gmail.com>
2021-09-19 20:37:21 -0700

    starting herku app

--- app_health/app.py
+++ app_health/app.py
@@ -52,7 +52,7 @@ CONTENT_STYLE = {
     # "color": "#33cc00",
 }
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+app = dash.Dash(__name__, suppress_callback_exceptions=True,external_stylesheets=[dbc.themes.DARKLY])
 
 
 def nav_bar():
